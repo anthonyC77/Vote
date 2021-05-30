b9f502770ca4962068d27d6456452daf0e869975
anthony.chassier@outlook.fr <anthony.chassier@outlook.fr>
2021-05-30 19:44:29 +0200

    Updates

---
+++ precache-manifest.f29b57dc96cf47704eaa1ecccef6f3a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8ae489c2938cc73b4455b351fee776",
+    "url": "/Vote/index.html"
+  },
+  {
+    "revision": "c2cb93bb990100354dbc",
+    "url": "/Vote/static/css/2.37c27f5c.chunk.css"
+  },
+  {
+    "revision": "dc40fa8d7d02e0c2c44c",
+    "url": "/Vote/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c2cb93bb990100354dbc",
+    "url": "/Vote/static/js/2.53462321.chunk.js"
+  },
+  {
+    "revision": "dc40fa8d7d02e0c2c44c",
+    "url": "/Vote/static/js/main.61cde608.chunk.js"
+  },
+  {
+    "revision": "82f850b182e24d96d271",
+    "url": "/Vote/static/js/runtime-main.c8185c66.js"
+  }
+]);
